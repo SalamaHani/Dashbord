9717e4a092385d8a69115d0513f5b54cd3a95227
Salama <122737355+SalamaHani@users.noreply.github.com>
2025-07-06 15:09:43 +0300

    test Dashbord

--- src/components/ChartAreaInteractive.jsx
+++ src/components/ChartAreaInteractive.jsx
@@ -28,109 +28,109 @@ import {
 export const description = "An interactive area chart";
 
 const chartData = [
-  { date: "2024-04-01", desktop: 222, mobile: 150 },
-  { date: "2024-04-02", desktop: 97, mobile: 180 },
-  { date: "2024-04-03", desktop: 167, mobile: 120 },
-  { date: "2024-04-04", desktop: 242, mobile: 260 },
-  { date: "2024-04-05", desktop: 373, mobile: 290 },
-  { date: "2024-04-06", desktop: 301, mobile: 340 },
-  { date: "2024-04-07", desktop: 245, mobile: 180 },
-  { date: "2024-04-08", desktop: 409, mobile: 320 },
-  { date: "2024-04-09", desktop: 59, mobile: 110 },
-  { date: "2024-04-10", desktop: 261, mobile: 190 },
-  { date: "2024-04-11", desktop: 327, mobile: 350 },
-  { date: "2024-04-12", desktop: 292, mobile: 210 },
-  { date: "2024-04-13", desktop: 342, mobile: 380 },
-  { date: "2024-04-14", desktop: 137, mobile: 220 },
-  { date: "2024-04-15", desktop: 120, mobile: 170 },
-  { date: "2024-04-16", desktop: 138, mobile: 190 },
-  { date: "2024-04-17", desktop: 446, mobile: 360 },
-  { date: "2024-04-18", desktop: 364, mobile: 410 },
-  { date: "2024-04-19", desktop: 243, mobile: 180 },
-  { date: "2024-04-20", desktop: 89, mobile: 150 },
-  { date: "2024-04-21", desktop: 137, mobile: 200 },
-  { date: "2024-04-22", desktop: 224, mobile: 170 },
-  { date: "2024-04-23", desktop: 138, mobile: 230 },
-  { date: "2024-04-24", desktop: 387, mobile: 290 },
-  { date: "2024-04-25", desktop: 215, mobile: 250 },
-  { date: "2024-04-26", desktop: 75, mobile: 130 },
-  { date: "2024-04-27", desktop: 383, mobile: 420 },
-  { date: "2024-04-28", desktop: 122, mobile: 180 },
-  { date: "2024-04-29", desktop: 315, mobile: 240 },
-  { date: "2024-04-30", desktop: 454, mobile: 380 },
-  { date: "2024-05-01", desktop: 165, mobile: 220 },
-  { date: "2024-05-02", desktop: 293, mobile: 310 },
-  { date: "2024-05-03", desktop: 247, mobile: 190 },
-  { date: "2024-05-04", desktop: 385, mobile: 420 },
-  { date: "2024-05-05", desktop: 481, mobile: 390 },
-  { date: "2024-05-06", desktop: 498, mobile: 520 },
-  { date: "2024-05-07", desktop: 388, mobile: 300 },
-  { date: "2024-05-08", desktop: 149, mobile: 210 },
-  { date: "2024-05-09", desktop: 227, mobile: 180 },
-  { date: "2024-05-10", desktop: 293, mobile: 330 },
-  { date: "2024-05-11", desktop: 335, mobile: 270 },
-  { date: "2024-05-12", desktop: 197, mobile: 240 },
-  { date: "2024-05-13", desktop: 197, mobile: 160 },
-  { date: "2024-05-14", desktop: 448, mobile: 490 },
-  { date: "2024-05-15", desktop: 473, mobile: 380 },
-  { date: "2024-05-16", desktop: 338, mobile: 400 },
-  { date: "2024-05-17", desktop: 499, mobile: 420 },
-  { date: "2024-05-18", desktop: 315, mobile: 350 },
-  { date: "2024-05-19", desktop: 235, mobile: 180 },
-  { date: "2024-05-20", desktop: 177, mobile: 230 },
-  { date: "2024-05-21", desktop: 82, mobile: 140 },
-  { date: "2024-05-22", desktop: 81, mobile: 120 },
-  { date: "2024-05-23", desktop: 252, mobile: 290 },
-  { date: "2024-05-24", desktop: 294, mobile: 220 },
-  { date: "2024-05-25", desktop: 201, mobile: 250 },
-  { date: "2024-05-26", desktop: 213, mobile: 170 },
-  { date: "2024-05-27", desktop: 420, mobile: 460 },
-  { date: "2024-05-28", desktop: 233, mobile: 190 },
-  { date: "2024-05-29", desktop: 78, mobile: 130 },
-  { date: "2024-05-30", desktop: 340, mobile: 280 },
-  { date: "2024-05-31", desktop: 178, mobile: 230 },
-  { date: "2024-06-01", desktop: 178, mobile: 200 },
-  { date: "2024-06-02", desktop: 470, mobile: 410 },
-  { date: "2024-06-03", desktop: 103, mobile: 160 },
-  { date: "2024-06-04", desktop: 439, mobile: 380 },
-  { date: "2024-06-05", desktop: 88, mobile: 140 },
-  { date: "2024-06-06", desktop: 294, mobile: 250 },
-  { date: "2024-06-07", desktop: 323, mobile: 370 },
-  { date: "2024-06-08", desktop: 385, mobile: 320 },
-  { date: "2024-06-09", desktop: 438, mobile: 480 },
-  { date: "2024-06-10", desktop: 155, mobile: 200 },
-  { date: "2024-06-11", desktop: 92, mobile: 150 },
-  { date: "2024-06-12", desktop: 492, mobile: 420 },
-  { date: "2024-06-13", desktop: 81, mobile: 130 },
-  { date: "2024-06-14", desktop: 426, mobile: 380 },
-  { date: "2024-06-15", desktop: 307, mobile: 350 },
-  { date: "2024-06-16", desktop: 371, mobile: 310 },
-  { date: "2024-06-17", desktop: 475, mobile: 520 },
-  { date: "2024-06-18", desktop: 107, mobile: 170 },
-  { date: "2024-06-19", desktop: 341, mobile: 290 },
-  { date: "2024-06-20", desktop: 408, mobile: 450 },
-  { date: "2024-06-21", desktop: 169, mobile: 210 },
-  { date: "2024-06-22", desktop: 317, mobile: 270 },
-  { date: "2024-06-23", desktop: 480, mobile: 530 },
-  { date: "2024-06-24", desktop: 132, mobile: 180 },
-  { date: "2024-06-25", desktop: 141, mobile: 190 },
-  { date: "2024-06-26", desktop: 434, mobile: 380 },
-  { date: "2024-06-27", desktop: 448, mobile: 490 },
-  { date: "2024-06-28", desktop: 149, mobile: 200 },
-  { date: "2024-06-29", desktop: 103, mobile: 160 },
-  { date: "2024-06-30", desktop: 446, mobile: 400 },
+  { date: "2024-04-01", Product: 222, custamar: 150 },
+  { date: "2024-04-02", Product: 97, custamar: 180 },
+  { date: "2024-04-03", Product: 167, custamar: 120 },
+  { date: "2024-04-04", Product: 242, custamar: 260 },
+  { date: "2024-04-05", Product: 373, custamar: 290 },
+  { date: "2024-04-06", Product: 301, custamar: 340 },
+  { date: "2024-04-07", Product: 245, custamar: 180 },
+  { date: "2024-04-08", Product: 409, custamar: 320 },
+  { date: "2024-04-09", Product: 59, custamar: 110 },
+  { date: "2024-04-10", Product: 261, custamar: 190 },
+  { date: "2024-04-11", Product: 327, custamar: 350 },
+  { date: "2024-04-12", Product: 292, custamar: 210 },
+  { date: "2024-04-13", Product: 342, custamar: 380 },
+  { date: "2024-04-14", Product: 137, custamar: 220 },
+  { date: "2024-04-15", Product: 120, custamar: 170 },
+  { date: "2024-04-16", Product: 138, custamar: 190 },
+  { date: "2024-04-17", Product: 446, custamar: 360 },
+  { date: "2024-04-18", Product: 364, custamar: 410 },
+  { date: "2024-04-19", Product: 243, custamar: 180 },
+  { date: "2024-04-20", Product: 89, custamar: 150 },
+  { date: "2024-04-21", Product: 137, custamar: 200 },
+  { date: "2024-04-22", Product: 224, custamar: 170 },
+  { date: "2024-04-23", Product: 138, custamar: 230 },
+  { date: "2024-04-24", Product: 387, custamar: 290 },
+  { date: "2024-04-25", Product: 215, custamar: 250 },
+  { date: "2024-04-26", Product: 75, custamar: 130 },
+  { date: "2024-04-27", Product: 383, custamar: 420 },
+  { date: "2024-04-28", Product: 122, custamar: 180 },
+  { date: "2024-04-29", Product: 315, custamar: 240 },
+  { date: "2024-04-30", Product: 454, custamar: 380 },
+  { date: "2024-05-01", Product: 165, custamar: 220 },
+  { date: "2024-05-02", Product: 293, custamar: 310 },
+  { date: "2024-05-03", Product: 247, custamar: 190 },
+  { date: "2024-05-04", Product: 385, custamar: 420 },
+  { date: "2024-05-05", Product: 481, custamar: 390 },
+  { date: "2024-05-06", Product: 498, custamar: 520 },
+  { date: "2024-05-07", Product: 388, custamar: 300 },
+  { date: "2024-05-08", Product: 149, custamar: 210 },
+  { date: "2024-05-09", Product: 227, custamar: 180 },
+  { date: "2024-05-10", Product: 293, custamar: 330 },
+  { date: "2024-05-11", Product: 335, custamar: 270 },
+  { date: "2024-05-12", Product: 197, custamar: 240 },
+  { date: "2024-05-13", Product: 197, custamar: 160 },
+  { date: "2024-05-14", Product: 448, custamar: 490 },
+  { date: "2024-05-15", Product: 473, custamar: 380 },
+  { date: "2024-05-16", Product: 338, custamar: 400 },
+  { date: "2024-05-17", Product: 499, custamar: 420 },
+  { date: "2024-05-18", Product: 315, custamar: 350 },
+  { date: "2024-05-19", Product: 235, custamar: 180 },
+  { date: "2024-05-20", Product: 177, custamar: 230 },
+  { date: "2024-05-21", Product: 82, custamar: 140 },
+  { date: "2024-05-22", Product: 81, custamar: 120 },
+  { date: "2024-05-23", Product: 252, custamar: 290 },
+  { date: "2024-05-24", Product: 294, custamar: 220 },
+  { date: "2024-05-25", Product: 201, custamar: 250 },
+  { date: "2024-05-26", Product: 213, custamar: 170 },
+  { date: "2024-05-27", Product: 420, custamar: 460 },
+  { date: "2024-05-28", Product: 233, custamar: 190 },
+  { date: "2024-05-29", Product: 78, custamar: 130 },
+  { date: "2024-05-30", Product: 340, custamar: 280 },
+  { date: "2024-05-31", Product: 178, custamar: 230 },
+  { date: "2024-06-01", Product: 178, custamar: 200 },
+  { date: "2024-06-02", Product: 470, custamar: 410 },
+  { date: "2024-06-03", Product: 103, custamar: 160 },
+  { date: "2024-06-04", Product: 439, custamar: 380 },
+  { date: "2024-06-05", Product: 88, custamar: 140 },
+  { date: "2024-06-06", Product: 294, custamar: 250 },
+  { date: "2024-06-07", Product: 323, custamar: 370 },
+  { date: "2024-06-08", Product: 385, custamar: 320 },
+  { date: "2024-06-09", Product: 438, custamar: 480 },
+  { date: "2024-06-10", Product: 155, custamar: 200 },
+  { date: "2024-06-11", Product: 92, custamar: 150 },
+  { date: "2024-06-12", Product: 492, custamar: 420 },
+  { date: "2024-06-13", Product: 81, custamar: 130 },
+  { date: "2024-06-14", Product: 426, custamar: 380 },
+  { date: "2024-06-15", Product: 307, custamar: 350 },
+  { date: "2024-06-16", Product: 371, custamar: 310 },
+  { date: "2024-06-17", Product: 475, custamar: 520 },
+  { date: "2024-06-18", Product: 107, custamar: 170 },
+  { date: "2024-06-19", Product: 341, custamar: 290 },
+  { date: "2024-06-20", Product: 408, custamar: 450 },
+  { date: "2024-06-21", Product: 169, custamar: 210 },
+  { date: "2024-06-22", Product: 317, custamar: 270 },
+  { date: "2024-06-23", Product: 480, custamar: 530 },
+  { date: "2024-06-24", Product: 132, custamar: 180 },
+  { date: "2024-06-25", Product: 141, custamar: 190 },
+  { date: "2024-06-26", Product: 434, custamar: 380 },
+  { date: "2024-06-27", Product: 448, custamar: 490 },
+  { date: "2024-06-28", Product: 149, custamar: 200 },
+  { date: "2024-06-29", Product: 103, custamar: 160 },
+  { date: "2024-06-30", Product: 446, custamar: 400 },
 ];
 
 const chartConfig = {
   visitors: {
     label: "Visitors",
   },
-  desktop: {
-    label: "Desktop",
+  Product: {
+    label: "Product",
     color: "oklch(0.80 0.18 152)",
   },
-  mobile: {
-    label: "Mobile",
+  custamar: {
+    label: "custamar",
     color: "oklch(0.64 0.21 25)",
   },
 };
@@ -156,7 +156,7 @@ export function ChartAreaInteractive() {
     <Card className="pt-0 w-full dark:bg-gray-800">
       <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
         <div className="grid flex-1 gap-1">
-          <CardTitle>Area Chart - Interactive</CardTitle>
+          <CardTitle>Area Chart - Products and Cusetamer is Ordors</CardTitle>
           <CardDescription>
             Showing total visitors for the last 3 months
           </CardDescription>
@@ -188,27 +188,27 @@ export function ChartAreaInteractive() {
         >
           <AreaChart data={filteredData}>
             <defs>
-              <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+              <linearGradient id="fillProduct" x1="0" y1="0" x2="0" y2="1">
                 <stop
                   offset="5%"
-                  stopColor="var(--color-desktop)"
+                  stopColor="var(--color-Product)"
                   stopOpacity={0.8}
                 />
                 <stop
                   offset="95%"
-                  stopColor="var(--color-desktop)"
+                  stopColor="var(--color-Product)"
                   stopOpacity={0.1}
                 />
               </linearGradient>
-              <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+              <linearGradient id="fillcustamar" x1="0" y1="0" x2="0" y2="1">
                 <stop
                   offset="5%"
-                  stopColor="var(--color-mobile)"
+                  stopColor="var(--color-custamar)"
                   stopOpacity={0.8}
                 />
                 <stop
                   offset="95%"
-                  stopColor="var(--color-mobile)"
+                  stopColor="var(--color-custamar)"
                   stopOpacity={0.1}
                 />
               </linearGradient>
@@ -243,17 +243,17 @@ export function ChartAreaInteractive() {
               }
             />
             <Area
-              dataKey="mobile"
+              dataKey="custamar"
               type="natural"
-              fill="url(#fillMobile)"
-              stroke="var(--color-mobile)"
+              fill="url(#fillcustamar)"
+              stroke="var(--color-custamar)"
               stackId="a"
             />
             <Area
-              dataKey="desktop"
+              dataKey="Product"
               type="natural"
-              fill="url(#fillDesktop)"
-              stroke="var(--color-desktop)"
+              fill="url(#fillProduct)"
+              stroke="var(--color-Product)"
               stackId="a"
             />
             <ChartLegend content={<ChartLegendContent />} />
